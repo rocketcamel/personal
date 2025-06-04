259a7fbfea94e6d72c6892e86e9f07b7c988d365
rocketcamel <luca_lise@icloud.com>
2025-06-03 18:47:43 -0700

    fix: theme respects localStorage

--- src/theme.ts
+++ src/theme.ts
@@ -5,10 +5,19 @@ type ThemeStore = {
   toggle: () => void;
 };
 
+let theme = localStorage.getItem("theme");
+
+if (!theme) {
+  theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+}
+
 Alpine.store("darkMode", {
-  on: true,
+  on: theme === "dark" ? true : false,
 
   toggle() {
     this.on = !this.on;
+    localStorage.setItem("theme", this.on ? "dark" : "light");
   },
 } as ThemeStore);
